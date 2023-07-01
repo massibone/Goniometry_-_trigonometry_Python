0b2ee20f2bd1af6dd8e3b04e1deca2fbd0e6943a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-07-01 13:12:41 +0200

    Update tractrix.py

--- Curves/tractrix.py
+++ Curves/tractrix.py
@@ -8,6 +8,19 @@ Other name: equitangential curve
 
 '''
 
+# Define the function for the tractrix curve
+def tractrix(t, a):
+    return a * np.log(np.abs((t + np.sqrt(t**2 + a**2)) / a))
+
+# Define the range of values for t
+t = np.linspace(-10, 10, 1000)
+
+# Set the parameter value for a
+a = 1
+
+# Calculate the x and y coordinates of the curve
+x = a * np.log(np.abs((t + np.sqrt(t**2 + a**2)) / a)) - t
+y = a * np.sqrt(np.abs(t**2 + a**2) - a**2)
 
 # Plot the curve
 plt.plot(x, y)
